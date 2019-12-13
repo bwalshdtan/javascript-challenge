1d4c89b8ef4fb5b6680ad78cd1d44b34911ccf5f
Brian M Walsh <bwalsh@Brian-Walshs-Computer-2.local>
2019-12-13 11:38:51 -0800

    Debug js and add css

--- static/app.js
+++ static/app.js
@@ -1,48 +1,53 @@
 let tableData = data;
 
-function tableDisplay(ufoSightings) {
-    let tbody = d3.select("tbody");
-    ufoSightings.forEach((ufoRecord) => {
-        let row = tbody.append("tr");
-        Object.entries(ufoRecord).forEach(([key, value]) => {
-            let cell = row.append("td");
-            cell.html(value);
-        });
-    });
-};
+function tableDisplay(ufos) {
+  let tbody = d3.select("tbody")
+  .selectAll("tr")
+  .data(tableData)
+  .enter()
+  .append("tr")
+  .html(function(d) {
+    return `<td>${d.datetime}</td><td>${d.city}</td><td>${d.state}</td><td>${d.country}</td><td>${d.shape}</td><td>${d.duration}</td><td>${d.comments}</td>`;
+  });
+}
 
-function deleteTbody() {
-    d3.select("tbody")
-    .selectAll("tr").remove()
-    .selectAll("td").remove();
-};
+ function deleteTbody() {
+     d3.select("tbody")
+     .selectAll("tr").remove()
+     .selectAll("td").remove();
+ };
 
-console.log(tableData);
-tableDisplay(tableData);
+ console.log(tableData);
+ tableDisplay(tableData);
 
 let button = d3.select("#filter-btn");
 
 button.on("click", function(event) {
     d3.event.preventDefault();
     deleteTbody();
-    let dateInput = d3.select("#datetime").property("value");
-
-    if (dateInput.trim() === "") {
-        let filterData = tableData;
+    var dateInput = d3.select("#datetime").property("value");
     
+    if (dateInput.trim() === "" ) {
+      
+      var filteredData = tableData;
     } else {
-        let filterData = tableData.filter(ufoSighting => 
-            ufoSighting.datetime === dateInput.trim());
+        
+      var filteredData = tableData.filter(ufo => 
+        ufo.datetime === dateInput.trim());
     };
-    if (filteredData.length === 0) {
-        d3.select("tbody")
+  
+    
+    if (filteredData.length == 0) {
+      d3.select("tbody")
         .append("tr")
         .append("td")
-            .attr("colspan", 7)
-            .html("<h4>No  Records Found</h4>");
+          .attr("colspan", 7)
+          .html("<h4>No Records Found</h4>");
     };
-    console.log(filterData);
-    tableDisplay(filterData);
+  
+    console.log(filteredData);
+    tableDisplay(filteredData);
+  });
 
- });
+
 
